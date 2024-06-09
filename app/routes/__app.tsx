517a40038a7fc5b8f26e14ec15710b5fd1873b90
DEEP-24 <deepkumarmendapara@gmail.com>
2024-06-09 17:16:00 -0500

    chore: fix layout

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,37 +1,24 @@
 import { ScrollArea } from "@mantine/core";
 import type { LoaderArgs, SerializeFrom } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Link, Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import { Form, Link, NavLink, Outlet } from "@remix-run/react";
 import {
   CircleUserIcon,
+  HistoryIcon,
   HomeIcon,
-  LineChartIcon,
-  MenuIcon,
-  Package2Icon,
-  PackageIcon,
-  ShoppingCartIcon,
-  UsersIcon,
+  ShoppingBagIcon,
 } from "lucide-react";
 import { Footer } from "~/components/Footer";
 import { Button } from "~/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { getAllProducts } from "~/lib/product.server";
 import { isAdmin, isCustomer, requireUserId } from "~/lib/session.server";
+import { cn } from "~/lib/utils";
 import { useOptionalUser } from "~/utils/hooks";
 
 export type AppLoaderData = SerializeFrom<typeof loader>;
@@ -56,29 +43,11 @@ export const loader = async ({ request }: LoaderArgs) => {
 };
 
 export default function AppLayout() {
-  return (
-    <>
-      <div className="flex h-full flex-col">
-        <HeaderComponent />
-        <ScrollArea classNames={{ root: "flex-1" }}>
-          <main>
-            <Outlet />
-          </main>
-        </ScrollArea>
-        <Footer />
-      </div>
-    </>
-  );
-}
-
-function HeaderComponent() {
-  const location = useLocation();
   const { user } = useOptionalUser();
-  const { isCustomer } = useLoaderData<typeof loader>();
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block h-screen overflow-hidden">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
@@ -88,33 +57,55 @@ function HeaderComponent() {
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                to="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  cn(
+                    "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-100",
+                    isActive &&
+                      "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-300 bg-gray-300",
+                  )
+                }
               >
                 <HomeIcon className="h-4 w-4" />
-                Dashboard
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                Home
+              </NavLink>
+              <NavLink
+                to="items"
+                className={({ isActive }) =>
+                  cn(
+                    "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-100",
+                    isActive &&
+                      "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-300 bg-gray-300",
+                  )
+                }
               >
-                <ShoppingCartIcon className="h-4 w-4" />
-                Orders
-              </Link>
-              <Link
-                to="#"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                <ShoppingBagIcon className="h-4 w-4" />
+                Items
+              </NavLink>
+              <NavLink
+                to="sale-details"
+                className={({ isActive }) =>
+                  cn(
+                    "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-100",
+                    isActive &&
+                      "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-300 bg-gray-300",
+                  )
+                }
               >
-                <PackageIcon className="h-4 w-4" />
-                Products{" "}
-              </Link>
+                <HistoryIcon className="h-4 w-4" />
+                Order History
+              </NavLink>
             </nav>
           </div>
         </div>
       </div>
-      <div className="flex flex-col">
-        <header className="flex h-14 items-center justify-end gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <div className="flex flex-col h-full overflow-hidden">
+        <header className="flex flex-shrink-0 h-14 items-center justify-end gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <div className="flex flex-col items-center justify-center">
+            <span>{user?.name}</span>
+            <span>{user?.email}</span>
+          </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild={true}>
               <Button variant="secondary" size="icon" className="rounded-full">
@@ -122,18 +113,28 @@ function HeaderComponent() {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="bg-white">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem>
+                <Form
+                  replace={true}
+                  action="/api/auth/logout"
+                  method="post"
+                  id="logout-form"
+                />
+                <button type="submit" form="logout-form">
+                  Logout
+                </button>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <Outlet />
-        </main>
+        <div className="flex-1 overflow-y-auto p-5">
+          <ScrollArea>
+            <main>
+              <Outlet />
+            </main>
+          </ScrollArea>
+        </div>
+        <Footer />
       </div>
     </div>
   );
