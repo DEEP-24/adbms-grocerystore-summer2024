967e23a4b0df01b67c8ddd202ae181f09421bb98
DEEP-24 <deepkumarmendapara@gmail.com>
2024-06-09 17:15:18 -0500

    update schema and seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -20,12 +20,16 @@ async function seed() {
         email: "user@app.com",
         password: await createPasswordHash("password"),
         role: Role.CUSTOMER,
+        phoneNo: "1234567890",
+        address: "123 Main St",
       },
       {
         name: "Jane Doe",
         email: "admin@app.com",
         password: await createPasswordHash("password"),
         role: Role.ADMIN,
+        phoneNo: "1234567890",
+        address: "123 Main St",
       },
     ],
   });
@@ -101,6 +105,83 @@ const products = [
     quantity: 10,
     price: 7.99,
     category: ["Vegetables"],
+  },
+  {
+    name: "Carrots (500gm)",
+    barcodeId: "6789",
+    description:
+      "The carrot is a root vegetable, typically orange in color, though purple, black, red, white, and yellow cultivars exist. Carrots are a domesticated form of the wild carrot, Daucus carota, native to Europe and Southwestern Asia.",
+    image:
+      "https://images.unsplash.com/photo-1598170845058-32b9d6a5da37?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2Fycm90c3xlbnwwfHwwfHx8MA%3D%3D",
+    quantity: 10,
+    price: 9.99,
+    category: ["Vegetables"],
+  },
+  {
+    name: "Milk (1L)",
+    barcodeId: "9876",
+    description:
+      "Milk is a nutrient-rich liquid food produced by the mammary glands of mammals. It is the primary source of nutrition for young mammals, including breastfed human infants before they are able to digest solid food.",
+    image:
+      "https://images.unsplash.com/photo-1634141510639-d691d86f47be?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1pbGt8ZW58MHx8MHx8fDA%3D",
+    quantity: 10,
+    price: 3.99,
+    category: ["Dairy"],
+  },
+  {
+    name: "Cheese (200gm)",
+    barcodeId: "8765",
+    description:
+      "Cheese is a dairy product, derived from milk and produced in wide ranges of flavors, textures, and forms by coagulation of the milk protein casein. It comprises proteins and fat from milk, usually the milk of cows, buffalo, goats, or sheep.",
+    image:
+      "https://images.unsplash.com/photo-1618164436241-4473940d1f5c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fENoZWVzZXxlbnwwfHwwfHx8MA%3D%3D",
+    quantity: 10,
+    price: 6.99,
+    category: ["Dairy"],
+  },
+  {
+    name: "Yogurt (500gm)",
+    barcodeId: "7654",
+    description:
+      "Yogurt, yoghurt, or yoghourt is a food produced by bacterial fermentation of milk. The bacteria used to make yogurt are known as yogurt cultures. Fermentation of sugars in the milk by these bacteria produces lactic acid, which acts on milk protein to give yogurt its texture and characteristic tart flavor.",
+    image:
+      "https://images.unsplash.com/photo-1571212515416-fef01fc43637?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9ndXJ0fGVufDB8fDB8fHww",
+    quantity: 10,
+    price: 4.99,
+    category: ["Dairy"],
+  },
+  {
+    name: "Apples (1kg)",
+    barcodeId: "3456",
+    description:
+      "The apple tree is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree and is the most widely grown species in the genus Malus.",
+    image:
+      "https://images.unsplash.com/photo-1630563451961-ac2ff27616ab?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fEFwcGxlc3xlbnwwfHwwfHx8MA%3D%3D",
+    quantity: 10,
+    price: 5.99,
+    category: ["Fruits"],
+  },
+  {
+    name: "Bananas (1kg)",
+    barcodeId: "2345",
+    description:
+      "A banana is an elongated, edible fruit produced by several kinds of large herbaceous flowering plants in the genus Musa. The fruit is variable in size, color, and firmness, but is usually elongated and curved, with soft flesh rich in starch covered with a rind, which may be green, yellow, red, purple, or brown when ripe.",
+    image:
+      "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QmFuYW5hc3xlbnwwfHwwfHx8MA%3D%3D",
+    quantity: 10,
+    price: 3.99,
+    category: ["Fruits"],
+  },
+  {
+    name: "Oranges (1kg)",
+    barcodeId: "4567",
+    description:
+      "The orange is the fruit of various citrus species in the family Rutaceae; it primarily refers to Citrus sinensis, which is also called sweet orange, to distinguish it from the related Citrus aurantium, referred to as bitter orange.",
+    image:
+      "https://images.unsplash.com/photo-1547514701-42782101795e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8T3Jhbmdlc3xlbnwwfHwwfHx8MA%3D%3D",
+    quantity: 10,
+    price: 4.99,
+    category: ["Fruits"],
   },
 ];
 
