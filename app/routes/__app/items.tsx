73f8d27564ee4965b9f1d0b43de42ed5de29042d
DEEP-24 <deepkumarmendapara@gmail.com>
2024-06-10 21:48:17 -0500

    integrated product carousel component into items page

--- app/routes/__app/items.tsx
+++ app/routes/__app/items.tsx
@@ -1,5 +1,4 @@
-import { Anchor } from "@mantine/core";
-import { Link } from "@remix-run/react";
+import ProductCarousel from "~/components/product-carousel";
 import { useAppData } from "~/utils/hooks";
 
 export default function Items() {
@@ -14,53 +13,16 @@ export default function Items() {
           </h2>
         </div>
 
-        {categories.map((category) => (
-          <div key={category.id}>
-            <h3 className="mt-3 text-lg font-semibold text-gray-900 bg-gray-100 w-full rounded-xl text-center p-2">
-              {category.name}
-            </h3>
-            <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 lg:grid-cols-4 lg:gap-x-8">
-              {category.products.map((product) => {
-                return (
-                  <Link
-                    to={`/product/${product.slug}`}
-                    key={product.id}
-                    className="hover:scale-105 transition-all"
-                  >
-                    <div className="mx-auto sm:mx-[unset] border-2 p-2 rounded-xl shadow-lg">
-                      <div className="h-48 overflow-hidden rounded-xl bg-gray-200 shadow lg:h-64">
-                        <img
-                          src={product.image}
-                          alt={product.name}
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-
-                      <p className="mt-1 text-sm font-medium text-gray-900 bg-slate-100 rounded-xl text-center p-1">
-                        Barcode ID: {product.barcodeId}
-                      </p>
-
-                      <div className="flex justify-between mt-1 p-2">
-                        <h3 className="text-sm text-gray-700">
-                          <Anchor
-                            to={`/product/${product.slug}`}
-                            prefetch="intent"
-                            component={Link}
-                          >
-                            {product.name}
-                          </Anchor>
-                        </h3>
-                        <p className="text-sm font-medium text-gray-900">
-                          Price: ${product.price}
-                        </p>
-                      </div>
-                    </div>
-                  </Link>
-                );
-              })}
+        {categories.map((category) => {
+          return (
+            <div key={category.id}>
+              <h3 className="mt-3 text-lg font-semibold text-gray-900 bg-gray-100 rounded-xl text-center p-2">
+                {category.name}
+              </h3>
+              <ProductCarousel products={category.products} />
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
