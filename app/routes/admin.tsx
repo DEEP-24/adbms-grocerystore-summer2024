aa9a7b7a53e2972a95658e2dc74b54e425ba9e7c
DEEP-24 <deepkumarmendapara@gmail.com>
2024-06-10 19:05:00 -0500

    chore: update admin layout

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,17 +1,23 @@
-import {
-  ArrowLeftOnRectangleIcon,
-  ArrowRightOnRectangleIcon,
-  UserPlusIcon,
-} from "@heroicons/react/24/solid";
-import { Anchor, Avatar, Divider, Menu, ScrollArea } from "@mantine/core";
+import { ScrollArea } from "@mantine/core";
 import type { LoaderArgs, SerializeFrom } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import type { ShouldReloadFunction } from "@remix-run/react";
-import { Form, Link, Outlet, useLocation } from "@remix-run/react";
-import appConfig from "app.config";
+import { Form, Link, NavLink, Outlet } from "@remix-run/react";
+import {
+  CircleUserIcon,
+  LayoutListIcon,
+  ShoppingBasketIcon,
+} from "lucide-react";
 import { Footer } from "~/components/Footer";
-import { TailwindContainer } from "~/components/TailwindContainer";
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { isCustomer, requireUser } from "~/lib/session.server";
+import { cn } from "~/lib/utils";
 import { useOptionalUser } from "~/utils/hooks";
 
 export type AppLoaderData = SerializeFrom<typeof loader>;
@@ -26,111 +32,92 @@ export const loader = async ({ request }: LoaderArgs) => {
 };
 
 export default function AppLayout() {
-  return (
-    <>
-      <div className="flex h-full flex-col">
-        <HeaderComponent />
-        <ScrollArea classNames={{ root: "flex-1 bg-gray-100" }}>
-          <main>
-            <Outlet />
-          </main>
-        </ScrollArea>
-        <Footer />
-      </div>
-    </>
-  );
-}
-
-function HeaderComponent() {
-  const location = useLocation();
   const { user } = useOptionalUser();
 
   return (
     <>
-      <Form
-        replace={true}
-        action="/api/auth/logout"
-        method="post"
-        id="logout-form"
-      />
-      <header className="h-[100px] p-4">
-        <TailwindContainer>
-          <div className="flex h-full w-full items-center justify-between">
-            <div className="flex flex-shrink-0 items-center gap-4">
-              <Anchor component={Link} to="/">
-                <img
-                  className="h-20 object-cover object-center"
-                  src={appConfig.logo}
-                  alt="Logo"
-                />
-              </Anchor>
+      <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block h-screen overflow-hidden">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <Link to="/" className="flex items-center gap-2 font-semibold">
+                <img src="/logo.png" alt="Logo" className="h-10 w-10" />
+                <span className="">Grocery Store</span>
+              </Link>
             </div>
-
-            <div className="flex items-center gap-4">
-              <Menu
-                position="bottom-start"
-                withArrow={true}
-                transition="pop-top-right"
-              >
-                <Menu.Target>
-                  <button>
-                    {user ? (
-                      <Avatar color="blue" size="md">
-                        {user.name.charAt(0)}
-                      </Avatar>
-                    ) : (
-                      <Avatar />
-                    )}
-                  </button>
-                </Menu.Target>
-
-                <Menu.Dropdown>
-                  {user ? (
-                    <>
-                      <Menu.Item disabled={true}>
-                        <div className="flex flex-col">
-                          <p>{user.name}</p>
-                          <p className="mt-0.5 text-sm">{user.email}</p>
-                        </div>
-                      </Menu.Item>
-                      <Divider />
-
-                      <Menu.Item
-                        icon={<ArrowLeftOnRectangleIcon className="h-4 w-4" />}
-                        type="submit"
-                        form="logout-form"
-                      >
-                        Logout
-                      </Menu.Item>
-                    </>
-                  ) : (
-                    <>
-                      <Menu.Item
-                        icon={<ArrowRightOnRectangleIcon className="h-4 w-4" />}
-                        component={Link}
-                        to={`/login?redirectTo=${encodeURIComponent(
-                          location.pathname,
-                        )}`}
-                      >
-                        Login
-                      </Menu.Item>
-                      <Menu.Item
-                        icon={<UserPlusIcon className="h-4 w-4" />}
-                        component={Link}
-                        to={`/register?redirectTo=${encodeURIComponent(
-                          location.pathname,
-                        )}`}
-                      >
-                        Create account
-                      </Menu.Item>
-                    </>
-                  )}
-                </Menu.Dropdown>
-              </Menu>
+            <div className="flex-1">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                <NavLink
+                  to="/admin/products"
+                  className={({ isActive }) =>
+                    cn(
+                      "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-100",
+                      isActive &&
+                        "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-300 bg-gray-300",
+                    )
+                  }
+                >
+                  <ShoppingBasketIcon className="h-4 w-4" />
+                  Products
+                </NavLink>
+                <NavLink
+                  to="/admin/categories"
+                  className={({ isActive }) =>
+                    cn(
+                      "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-100",
+                      isActive &&
+                        "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-300 bg-gray-300",
+                    )
+                  }
+                >
+                  <LayoutListIcon className="h-4 w-4" />
+                  Categories
+                </NavLink>
+              </nav>
             </div>
           </div>
-        </TailwindContainer>
-      </header>
+        </div>
+        <div className="flex flex-col h-full overflow-hidden">
+          <header className="flex flex-shrink-0 h-14 items-center justify-end gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <div className="flex flex-col items-center justify-center">
+              <span>{user?.name}</span>
+              <span>{user?.email}</span>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild={true}>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full"
+                >
+                  <CircleUserIcon className="h-5 w-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="bg-white">
+                <DropdownMenuItem>
+                  <Form
+                    replace={true}
+                    action="/api/auth/logout"
+                    method="post"
+                    id="logout-form"
+                  />
+                  <button type="submit" form="logout-form">
+                    Logout
+                  </button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </header>
+          <div className="flex-1 overflow-y-auto p-5">
+            <ScrollArea>
+              <main>
+                <Outlet />
+              </main>
+            </ScrollArea>
+          </div>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
